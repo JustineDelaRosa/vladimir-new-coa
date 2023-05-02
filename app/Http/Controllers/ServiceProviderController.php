ba59793b5a142208c484b42fb7bbf1c779b89373
JustineDelaRosa <60209838+JustineDelaRosa@users.noreply.github.com>
2023-05-02 13:15:36 +0800

    service provider

--- app/Http/Controllers/ServiceProviderController.php
+++ app/Http/Controllers/ServiceProviderController.php
@@ -38,7 +38,7 @@ class ServiceProviderController extends Controller
             "service_provider_name" => $service_provider_name,
             "is_active" => 1
         ]);
-        
+
         return response()->json(['message' => 'Successfully Created!', 'data' => $create]);
     }
 
@@ -50,11 +50,11 @@ class ServiceProviderController extends Controller
      */
     public function show(ServiceProviderRequest $request, $id)
     {
-       $ServiceProvider = ServiceProvider::query();
-       if(!$ServiceProvider->where('id', $id)->exists()){
-        return response()->json(['error' => 'Service ProviderRoute Not Found'], 404);
-       }
-       return $ServiceProvider->where('id', $id)->first();
+        $ServiceProvider = ServiceProvider::query();
+        if (!$ServiceProvider->where('id', $id)->exists()) {
+            return response()->json(['error' => 'Service ProviderRoute Not Found'], 404);
+        }
+        return $ServiceProvider->where('id', $id)->first();
     }
 
     /**
@@ -67,15 +67,14 @@ class ServiceProviderController extends Controller
     public function update(ServiceProviderRequest $request, $id)
     {
         $service_provider_name = $request->service_provider_name;
-        if(!ServiceProvider::where('id', $id)->exists()){
+        if (!ServiceProvider::where('id', $id)->exists()) {
             return response()->json(['error' => 'Service Provider Route Not Found'], 404);
         }
-        if(ServiceProvider::where('id',$id)->where('service_provider_name', $service_provider_name)->exists()){
+        if (ServiceProvider::where('id', $id)->where('service_provider_name', $service_provider_name)->exists()) {
             return response()->json(['message' => 'No Changes'], 200);
         }
         $update = ServiceProvider::where('id', $id)->update(['service_provider_name' => $service_provider_name]);
         return response()->json(['message' => 'Successfully Updated!'], 200);
-    
     }
 
     /**
@@ -89,79 +88,73 @@ class ServiceProviderController extends Controller
         //
     }
 
-    public function archived(ServiceProviderRequest $request, $id){
+    public function archived(ServiceProviderRequest $request, $id)
+    {
 
-        $status = $request->status; 
+        $status = $request->status;
         $ServiceProvider = ServiceProvider::query();
-        if(!$ServiceProvider->withTrashed()->where('id', $id)->exists()){
+        if (!$ServiceProvider->withTrashed()->where('id', $id)->exists()) {
             return response()->json(['error' => 'Service Provider Route Not Found'], 404);
         }
 
-        if(CategoryList::where('service_provider_id', $id)->exists()){
-            if($status == true){
-                return response()->json(['message' => 'No Changes'],200);
-            }
-            else{
-                return response()->json(['message' => 'Unable to Archived!'],409);
+        if (ServiceProvider::where('id', $id)->exists()) {
+            if ($status == true) {
+                return response()->json(['message' => 'No Changes'], 200);
+            } else {
+                return response()->json(['message' => 'Unable to Archived!'], 409);
             }
         }
 
-        if($status == false){
-            if(!ServiceProvider::where('id',$id)->where('is_active', true)->exists()){
+        if ($status == false) {
+            if (!ServiceProvider::where('id', $id)->where('is_active', true)->exists()) {
                 return response()->json(['message' => 'No Changes'], 200);
-            }
-            else{
-                if(!CategoryList::where('service_provider_id', $id)->exists()){
+            } else {
+                if (!ServiceProvider::where('service_provider_id', $id)->exists()) {
                     $updateStatus = $ServiceProvider->where('id', $id)->update(['is_active' => false]);
-                    $ServiceProvider->where('id',$id)->delete();
+                    $ServiceProvider->where('id', $id)->delete();
                     return response()->json(['message' => 'Successfully Deactived!'], 200);
                 }
                 return response()->json(['message' => 'Unable to Archived!, Service Provider was tagged!']);
             }
         }
-        if($status == true){
-            if(ServiceProvider::where('id',$id)->where('is_active', true)->exists()){
+        if ($status == true) {
+            if (ServiceProvider::where('id', $id)->where('is_active', true)->exists()) {
                 return response()->json(['message' => 'No Changes'], 200);
-            }
-            else{              
-                $restoreUser = $ServiceProvider->withTrashed()->where('id',$id)->restore();
-                $updateStatus = $ServiceProvider->update(['is_active' => true]); 
+            } else {
+                $restoreUser = $ServiceProvider->withTrashed()->where('id', $id)->restore();
+                $updateStatus = $ServiceProvider->update(['is_active' => true]);
                 return response()->json(['message' => 'Successfully Activated!'], 200);
             }
-
         }
-
     }
 
-    public function search(Request $request){
+    public function search(Request $request)
+    {
         $search = $request->query('search');
         $limit = $request->query('limit');
         $page = $request->get('page');
         $status = $request->query('status');
-        if($status == NULL ){
+        if ($status == NULL) {
             $status = 1;
         }
-        if($status == "active"){
+        if ($status == "active") {
             $status = 1;
         }
-        if($status == "deactivated"){
+        if ($status == "deactivated") {
             $status = 0;
         }
-        if($status != "active" || $status != "deactivated"){
+        if ($status != "active" || $status != "deactivated") {
             $status = 1;
         }
         $ServiceProvider = ServiceProvider::withTrashed()
-        ->where(function($query) use($status){
-            $query->where('is_active', $status);
-        })
-        ->where(function($query) use($search){
-            $query->where('service_provider_name', 'LIKE', "%{$search}%" );
-        })
-        ->orderby('created_at', 'DESC')
-        ->paginate($limit);
+            ->where(function ($query) use ($status) {
+                $query->where('is_active', $status);
+            })
+            ->where(function ($query) use ($search) {
+                $query->where('service_provider_name', 'LIKE', "%{$search}%");
+            })
+            ->orderby('created_at', 'DESC')
+            ->paginate($limit);
         return $ServiceProvider;
-    } 
-
-
-
+    }
 }
