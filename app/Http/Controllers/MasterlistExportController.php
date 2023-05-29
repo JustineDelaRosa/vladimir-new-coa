e14f4b733121d1737b340ffc921b3044d5530ea5
JustineDelaRosa <60209838+JustineDelaRosa@users.noreply.github.com>
2023-05-29 16:54:53 +0800

    adjustment to export no laravel excel

--- app/Http/Controllers/MasterlistExportController.php
+++ app/Http/Controllers/MasterlistExportController.php
@@ -25,9 +25,26 @@ class MasterlistExportController extends Controller
 
 
 
-        if($search == null && $startDate == null && $endDate == null){
-        $fixed_assets = FixedAsset::with('formula')->get();
-        return $this->refactorExport($fixed_assets);
+        if($startDate != null && $endDate != null){
+            $fixed_assets = FixedAsset::withTrashed()->with(
+                [
+                    'formula' => function ($query) {
+                        $query->withTrashed();
+                    },
+                    'division' => function ($query) {
+                        $query->withTrashed()->select('id', 'division_name');
+                    },
+                    'majorCategory' => function ($query) {
+                        $query->withTrashed()->select('id', 'major_category_name');
+                    },
+                    'minorCategory' => function ($query) {
+                        $query->withTrashed()->select('id', 'minor_category_name');
+                    },
+                ]
+            )
+                ->whereBetween('created_at', [$startDate, $endDate])
+                ->get();
+            return $this->refactorExport($fixed_assets);
         }
 
         $fixedAsset = FixedAsset::withTrashed()->with(
@@ -47,40 +64,40 @@ class MasterlistExportController extends Controller
             ]
         )
             ->Where(function ($query) use ($search, $startDate, $endDate) {
-                $query->Where('capex', $search)
-                    ->orWhere('project_name', $search)
-                    ->orWhere('vladimir_tag_number', $search)
-                    ->orWhere('tag_number', $search)
-                    ->orWhere('tag_number_old', $search)
-                    ->orWhere('type_of_request', $search)
-                    ->orWhere('accountability', $search)
-                    ->orWhere('accountable', $search)
-                    ->orWhere('brand', $search)
-                    ->orWhere('depreciation_method', $search);
+                $query->Where('capex', 'LIKE', '%'.$search.'%')
+                    ->orWhere('project_name', 'LIKE', '%'.$search.'%')
+                    ->orWhere('vladimir_tag_number', 'LIKE', '%'.$search.'%')
+                    ->orWhere('tag_number', 'LIKE', '%'.$search.'%')
+                    ->orWhere('tag_number_old', 'LIKE', '%'.$search.'%')
+                    ->orWhere('type_of_request', 'LIKE', '%'.$search.'%')
+                    ->orWhere('accountability', 'LIKE', '%'.$search.'%')
+                    ->orWhere('accountable', 'LIKE', '%'.$search.'%')
+                    ->orWhere('brand', 'LIKE', '%'.$search.'%')
+                    ->orWhere('depreciation_method', 'LIKE', '%'.$search.'%');
                 $query->orWhereHas('majorCategory', function ($query) use ($search) {
-                    $query->where('major_category_name', $search);
+                    $query->where('major_category_name', 'LIKE', '%'.$search.'%');
                 });
                 $query->orWhereHas('minorCategory', function ($query) use ($search) {
-                    $query->where('minor_category_name',$search );
+                    $query->where('minor_category_name', 'LIKE','%'.$search.'%');
                 });
                 $query->orWhereHas('division', function ($query) use ($search) {
-                    $query->where('division_name', $search );
+                    $query->where('division_name', 'LIKE', '%{$search}%');
                 });
                 $query->orWhereHas('location', function ($query) use ($search) {
-                    $query->where('location_name', $search);
+                    $query->where('location_name', 'LIKE', '%'.$search.'%');
                 });
                 $query->orWhereHas('company', function ($query) use ($search) {
-                    $query->where('company_name', $search);
+                    $query->where('company_name', 'LIKE', '%'.$search.'%');
                 });
                 $query->orWhereHas('department', function ($query) use ($search) {
-                    $query->where('department_name', $search );
+                    $query->where('department_name', 'LIKE', '%'.$search.'%');
                 });
                 $query->orWhereHas('accountTitle', function ($query) use ($search) {
-                    $query->where('account_title_name', $search);
+                    $query->where('account_title_name', 'LIKE', '%'.$search.'%');
                 });
-                $query->orWhere(function ($query) use ($startDate, $endDate) {
-                    $query->whereBetween('created_at', [$startDate, $endDate]);
-                });
+//                $query->orWhere(function ($query) use ($startDate, $endDate) {
+//                    $query->whereBetween('created_at', [$startDate, $endDate]);
+//                });
 
         })
             ->orderBy('id', 'ASC')
