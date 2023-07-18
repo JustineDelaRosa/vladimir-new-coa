c9c07385c0f98f0a42a85763c65066d208f47e42
JustineDelaRosa <60209838+JustineDelaRosa@users.noreply.github.com>
2023-07-18 10:01:12 +0800

    Add return types to methods in CapexRequest
    The methods authorize(), rules() and messages() in CapexRequest.php have been updated to specify return types of boolean and array accordingly. This change enhances the type-safety of the code and allows IDEs to provide better auto-completion and error checking.

--- app/Http/Requests/Capex/CapexRequest.php
+++ app/Http/Requests/Capex/CapexRequest.php
@@ -11,7 +11,7 @@ class CapexRequest extends FormRequest
      *
      * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
         return true;
     }
@@ -21,7 +21,7 @@ class CapexRequest extends FormRequest
      *
      * @return array
      */
-    public function rules()
+    public function rules(): array
     {
 
         if($this->isMethod('post')){
@@ -47,7 +47,7 @@ class CapexRequest extends FormRequest
         }
     }
 
-    public function messages()
+    public function messages(): array
     {
         return [
             'capex.required' => 'Capex is required.',
@@ -58,5 +58,4 @@ class CapexRequest extends FormRequest
             'status.boolean' => 'Status should be boolean.',
         ];
     }
-
 }
