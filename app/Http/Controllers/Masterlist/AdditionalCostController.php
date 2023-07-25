ecbb76f3e568ef8c45ae61761ff8bdf35d34e957
JustineDelaRosa <60209838+JustineDelaRosa@users.noreply.github.com>
2023-07-25 15:19:50 +0800

    adding additional cost

---
+++ app/Http/Controllers/Masterlist/AdditionalCostController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Masterlist;
+
+use App\Http\Controllers\Controller;
+use App\Models\AdditionalCost;
+use Illuminate\Http\Request;
+
+class AdditionalCostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\AdditionalCost  $additionalCost
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AdditionalCost $additionalCost)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\AdditionalCost  $additionalCost
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AdditionalCost $additionalCost)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\AdditionalCost  $additionalCost
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AdditionalCost $additionalCost)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\AdditionalCost  $additionalCost
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AdditionalCost $additionalCost)
+    {
+        //
+    }
+}
