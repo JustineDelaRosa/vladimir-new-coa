28e5032ff598016680d742484d639d22264a51e4
JustineDelaRosa <60209838+JustineDelaRosa@users.noreply.github.com>
2023-07-31 13:11:21 +0800

    Refactor AdditionalCostController to use Repository pattern
    Refactored `AdditionalCostController` to make use of the repository pattern instead of directly using eloquent in the controller.
    Changes include:
    - Use of `AdditionalCostRepository` for easier maintainability and readability
    - Improved http responses to return JsonResponse
    - Expanded on create, read, update methods and implemented validation
    This refactoring enhances the separation of concerns in the codebase and make it more scalable.

--- app/Http/Controllers/Masterlist/AdditionalCostController.php
+++ app/Http/Controllers/Masterlist/AdditionalCostController.php
@@ -3,84 +3,97 @@
 namespace App\Http\Controllers\Masterlist;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\AdditionalCost\AdditionalCostRequest;
 use App\Models\AdditionalCost;
+use App\Models\Department;
+use App\Repositories\AdditionalCostRepository;
 use Illuminate\Http\Request;
 
 class AdditionalCostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    protected $additionalCostRepository;
+
+    public function __construct()
     {
-        //
+        $this->additionalCostRepository = new AdditionalCostRepository();
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function create()
+    public function index()
     {
-        //
+        $additionalCost = AdditionalCost::with('formula')->get();
+        return response()->json([
+            'message' => 'Successfully fetched all additional cost!',
+            'data' => $this->additionalCostRepository->transformAdditionalCost($additionalCost)
+        ], 200);
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function store(Request $request)
+    public function store(AdditionalCostRequest $request)
     {
-        //
+        $departmentQuery = Department::where('id', $request->department_id)->first();
+        $additionalCost = $this->additionalCostRepository->storeAdditionalCost($request->all(), $departmentQuery);
+
+        return response()->json([
+            'message' => 'Additional Cost successfully created!',
+            'data' => $additionalCost,
+        ], 201);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\AdditionalCost  $additionalCost
-     * @return \Illuminate\Http\Response
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function show(AdditionalCost $additionalCost)
+    public function show($id)
     {
-        //
-    }
+        $additional_cost = AdditionalCost::withTrashed()->with('formula')->where('id', $id)->first();
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\AdditionalCost  $additionalCost
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(AdditionalCost $additionalCost)
-    {
-        //
+        if(!$additional_cost) {
+            return response()->json([
+                'message' => 'Additional Cost route not found!',
+            ], 404);
+        }
+
+        return response()->json([
+            'message' => 'Successfully fetched additional cost!',
+            'data' => $this->additionalCostRepository->transformSingleAdditionalCost($additional_cost),
+        ], 200);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\AdditionalCost  $additionalCost
-     * @return \Illuminate\Http\Response
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function update(Request $request, AdditionalCost $additionalCost)
+    public function update(AdditionalCostRequest $request, $id)
     {
-        //
+        $request->validated();
+        $departmentQuery = Department::where('id', $request->department_id)->first();
+        $additionalCost = AdditionalCost::where('id', $id)->first();
+        if($additionalCost) {
+            $additionalCost = $this->additionalCostRepository->updateAdditionalCost($request->all(), $departmentQuery, $id);
+            return response()->json([
+                'message' => 'Additional Cost successfully updated!',
+                'data' => $additionalCost->load('formula'),
+            ], 201);
+        }else{
+            return response()->json([
+                'message' => 'Additional Cost not found!',
+            ], 404);
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\AdditionalCost  $additionalCost
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(AdditionalCost $additionalCost)
-    {
-        //
-    }
 }
