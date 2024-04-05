d9c854dd5ad7140537b7fe62b81b6ea6131d2813
JustineDelaRosa <60209838+JustineDelaRosa@users.noreply.github.com>
2024-04-05 09:12:46 +0800

    adjust migration of user to new COA

--- database/migrations/2023_10_19_000000_create_users_table.php
+++ database/migrations/2023_10_19_000000_create_users_table.php
@@ -20,8 +20,12 @@ class CreateUsersTable extends Migration
             $table->string('lastname');
             $table->string('username');
             $table->string('password');
+            $table->unsignedInteger('company_id');
+            $table->unsignedInteger('business_unit_id');
+            $table->unsignedInteger('department_id');
             $table->unsignedInteger('unit_id')->nullable();
             $table->unsignedInteger('subunit_id')->nullable();
+            $table->unsignedInteger('location_id');
             $table->boolean('is_active');
             $table->unsignedInteger('role_id');
             $table->rememberToken();
@@ -29,16 +33,25 @@ class CreateUsersTable extends Migration
             $table->softDeletes($column='deleted_at',$precision=0);
             $table->foreign('role_id')
             ->references('id')
-            ->on('role_management')
-            ->onDelete('cascade');
+            ->on('role_management');
+            $table->foreign('company_id')
+            ->references('id')
+            ->on('companies');
+            $table->foreign('business_unit_id')
+            ->references('id')
+            ->on('business_units');
+            $table->foreign('department_id')
+            ->references('id')
+            ->on('departments');
             $table->foreign('unit_id')
             ->references('id')
-            ->on('unit')
-            ->onDelete('cascade');
+            ->on('units');
             $table->foreign('subunit_id')
             ->references('id')
-            ->on('sub_units')
-            ->onDelete('cascade');
+            ->on('sub_units');
+            $table->foreign('location_id')
+            ->references('id')
+            ->on('locations');
         });
     }
 
