54be97c2ac47fb92062b94f28de46e7b84cd2292
JustineDelaRosa <60209838+JustineDelaRosa@users.noreply.github.com>
2024-05-24 08:28:23 +0800

    reusable traits for asset movement

--- app/Traits/AssetMovementHandler.php
+++ app/Traits/AssetMovementHandler.php
@@ -4,7 +4,9 @@ namespace App\Traits;
 
 use App\Models\Approvers;
 use App\Models\FixedAsset;
+use App\Models\RoleManagement;
 use App\Models\TransferApproval;
+use App\Models\User;
 use Essa\APIToolKit\Api\ApiResponse;
 
 trait AssetMovementHandler
@@ -231,6 +233,24 @@ trait AssetMovementHandler
             ->deleteFileAfterSend(true);
     }
 
+    public function isUserFa(): bool
+    {
+        $user = auth('sanctum')->user()->id;
+        $faRoleIds = RoleManagement::whereIn('role_name', ['Fixed Assets', 'Fixed Asset Associate'])->pluck('id');
+        $user = User::where('id', $user)->whereIn('role_id', $faRoleIds)->exists();
+        return $user ? 1 : 0;
+    }
+
+    public function isRequestApproved($uniqueNumberValue, $uniqueNumber, $model, $approvalModelName): bool
+    {
+        $request = $model::where([
+            $uniqueNumber => $uniqueNumberValue,
+            'status' => 'Approved'
+        ])->exists();
+
+        return $request ? 1 : 0;
+    }
+
     public function requestAction($action, $uniqueNumberValue, $uniqueNumber, $model, $approvalModelName, $remarks = null)
     {
         $user = auth('sanctum')->user()->id;
@@ -242,13 +262,19 @@ trait AssetMovementHandler
             ->where('approver_id', $approverId)
             ->where('status', 'For Approval')
             ->first();
-        if (!$isApprover) {
-            return $this->responseUnprocessable('Invalid Action');
-        }
 
+        if (!$this->isUserFa() && !$this->isRequestApproved($uniqueNumberValue, $uniqueNumber, $model, $approvalModelName)) {
+            if (!$isApprover) {
+                return $this->responseNotFound('Request not found');
+            }
+        }
 
         switch (strtolower($action)) {
             case 'approve':
+                if ($this->isUserFa() && $this->isRequestApproved($uniqueNumberValue, $uniqueNumber, $model, $approvalModelName)) {
+                    $this->faApproval($uniqueNumberValue, $uniqueNumber, $model);
+                    break;
+                }
                 $this->approveRequest($uniqueNumberValue, $uniqueNumber, $model, $approvalModelName, $nextApprover->layer ?? null);
                 break;
             case 'return':
@@ -262,23 +288,37 @@ trait AssetMovementHandler
 
     public function approveRequest($uniqueNumberValue, $uniqueNumber, $model, $approvalModelName, $nextApprover)
     {
-        $fixedAssets = $model::where($uniqueNumber, $uniqueNumberValue)->get();
-        $approvalModelName::where($uniqueNumber, $uniqueNumberValue)
-            ->where('status', 'For Approval')
-            ->update(['status' => 'Approved']);
+        // Update the status of the approval model to 'Approved'
+        $approvalModelName::where($uniqueNumber, $uniqueNumberValue)->where('status', 'For Approval')->update(['status' => 'Approved']);
+
+        // If the approval model exists
         if ($approvalModelName) {
+            // If there is a next approver
             if ($nextApprover) {
-                $model::where($uniqueNumber, $uniqueNumberValue)
-                    ->update(['status' => 'For Approval of Approver ' . $nextApprover]);
-                $approvalModelName::where($uniqueNumber, $uniqueNumberValue)
-                    ->where('layer', $nextApprover)
-                    ->update(['status' => 'For Approval']);
+                // Update the status of the model to 'For Approval of Approver {nextApprover}'
+                $model::where($uniqueNumber, $uniqueNumberValue)->update(['status' => 'For Approval of Approver ' . $nextApprover]);
+
+                // Update the status of the approval model to 'For Approval'
+                $approvalModelName::where($uniqueNumber, $uniqueNumberValue)->where('layer', $nextApprover)->update(['status' => 'For Approval']);
             } else {
-                $model::where($uniqueNumber, $uniqueNumberValue)
-                    ->update(['status' => 'Approved']);
-                $this->addToAssetMovementHistory($fixedAssets->pluck('fixed_asset_id')->toArray(), $fixedAssets[0]->created_by_id);
-                $this->saveToFaTable($uniqueNumberValue, $uniqueNumber, $model, 'transfer');
+                // If there is no next approver, update the status of the model to 'Approved'
+                $model::where($uniqueNumber, $uniqueNumberValue)->update(['status' => 'Approved']);
             }
+        }
+    }
+
+    public function faApproval($uniqueNumberValue, $uniqueNumber, $model)
+    {
+        $fixedAssets = $model::where($uniqueNumber, $uniqueNumberValue)->get();
+        $isFaApproved = $fixedAssets->where('is_fa_approved', 0)->where('status', 'Approved')->first();
+        if ($isFaApproved) {
+            $model::where($uniqueNumber, $uniqueNumberValue)->update(['is_fa_approved' => true]);
+
+            // Add to asset movement history
+            $this->addToAssetMovementHistory($fixedAssets->pluck('fixed_asset_id')->toArray(), $fixedAssets[0]->created_by_id);
+
+            // Save to FA table
+            $this->saveToFaTable($uniqueNumberValue, $uniqueNumber, $model, 'transfer');
         }
     }
 
