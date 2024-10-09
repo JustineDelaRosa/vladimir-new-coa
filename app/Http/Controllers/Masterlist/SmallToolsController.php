720cb6081a6dab0cd80ae4bc8a23d9d0872ea39c
Justine Dela Rosa <justinedelarosa06@gmail.com>
2024-10-09 13:44:44 +0800

    Add item and uom relationships to SmallTools
    Enhanced the SmallTools model by integrating relationships with UnitOfMeasure and Item models. Updated SmallToolsController to include item and uom data in its responses and handled validation of item existence during the store process.

--- app/Http/Controllers/Masterlist/SmallToolsController.php
+++ app/Http/Controllers/Masterlist/SmallToolsController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Masterlist;
 
 use App\Http\Controllers\Controller;
 use App\Models\BusinessUnit;
+use App\Models\Item;
 use App\Models\SmallTools;
+use App\Models\UnitOfMeasure;
 use Illuminate\Http\Request;
 
 class SmallToolsController extends Controller
@@ -15,16 +17,47 @@ class SmallToolsController extends Controller
         $smallToolsStatus = $request->status ?? 'active';
         $isActiveStatus = ($smallToolsStatus === 'deactivated') ? 0 : 1;
 
-        $smallTools = SmallTools::where('is_active', $isActiveStatus)
+        $smallTools = SmallTools::with(['item', 'uom'])->where('is_active', $isActiveStatus)
             ->orderBy('created_at', 'DESC')
             ->useFilters()
             ->dynamicPaginate();
+
+        $smallTools->transform(function ($smallTool) {
+            return [
+                'id' => $smallTool->id,
+                'sync_id' => $smallTool->sync_id,
+                'uom' => [
+                    'id' => $smallTool->uom->sync_id,
+                    'uom_code' => $smallTool->uom->uom_code,
+                    'uom_name' => $smallTool->uom->uom_name,
+                ],
+                'small_tool_code' => $smallTool->small_tool_code,
+                'small_tool_name' => $smallTool->small_tool_name,
+                'is_active' => $smallTool->is_active,
+                'items' => $smallTool->item->map(function ($item) {
+                    return [
+                        'id' => $item->sync_id,
+                        'sync_id' => $item->sync_id,
+                        'item_code' => $item->item_code,
+                        'item_name' => $item->item_name,
+                        'is_active' => $item->is_active,
+                    ];
+                })
+            ];
+        });
         return $smallTools;
     }
 
 
     public function store(Request $request)
     {
+
+        $item = Item::all()->isEmpty();
+        if ($item) {
+//            return response()->json(['message' => 'Company Data not Ready'], 422);
+            return $this->responseUnprocessable('Item Data not Ready');
+        }
+
         $smallToolsData = $request->input('result');
         if (empty($request->all()) || empty($request->input('result'))) {
 //            return response()->json(['message' => 'Data not Ready']);
@@ -32,6 +65,9 @@ class SmallToolsController extends Controller
         }
 
         foreach ($smallToolsData as $smallTool) {
+
+//            return             $itemIds = array_column($smallTool['small_tools'], 'id');
+            $uomId = UnitOfMeasure::where('sync_id', $smallTool['uom_id'])->first()->id;
             $sync_id = $smallTool['id'];
             $code = $smallTool['code'];
             $name = $smallTool['name'];
@@ -42,11 +78,19 @@ class SmallToolsController extends Controller
                     'sync_id' => $sync_id,
                 ],
                 [
+                    'uom_id' => $uomId,
                     'small_tool_code' => $code,
                     'small_tool_name' => $name,
                     'is_active' => $is_active == NULL ? 1 : 0,
                 ],
             );
+            $itemIds = array_column($smallTool['small_tools'], 'small_tools_id');
+            foreach ($itemIds as $itemId) {
+                if (!Item::where('sync_id', $itemId)->exists()) {
+                    return $this->responseUnprocessable('Some of item is missing sync the item first!');
+                }
+            }
+            $sync->item()->sync($itemIds);
         }
 //        return response()->json(['message' => 'Successfully Synced!']);
         return $this->responseSuccess('Successfully Synced!');
--- app/Models/SmallTools.php
+++ app/Models/SmallTools.php
@@ -10,7 +10,27 @@ use Illuminate\Database\Eloquent\Model;
 class SmallTools extends Model
 {
     use HasFactory, Filterable;
+
     protected string $default_filters = SmallToolFilters::class;
     protected $guarded = [];
+
+
+    public function uom()
+    {
+        return $this->belongsTo(UnitOfMeasure::class, 'uom_id', 'sync_id');
+    }
+
+    public function item()
+    {
+        return $this->belongsToMany(
+            Item::class,
+            "small_tools_item",
+            "small_tool_sync_id",
+            "item_sync_id",
+            "sync_id",
+            "sync_id"
+        );
+    }
+
 
 }
