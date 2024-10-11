63decde76e29ef4334dc46072d1594094044f3ee
Justine Dela Rosa <justinedelarosa06@gmail.com>
2024-10-11 08:22:35 +0800

    Add new Ymir PR API configuration variables
    Added 'ymir_put_pr_api_url' and 'ymir_put_pr_api_token' to the Ymir API config. This enhancement will allow the application to interact with the PR API endpoints, improving functionality for pull request operations.

--- config/ymir-api.php
+++ config/ymir-api.php
@@ -6,4 +6,7 @@ return [
 
     'ymir_put_rr_api_token' => env('YMIRPUTRRAPI_TOKEN'),
     'ymir_put_rr_api_url' => env('YMIRPUTRRAPI_URL'),
+
+    'ymir_put_pr_api_url' => env('UPDATEYMIRPR_URL'),
+    'ymir_put_pr_api_token' => env('YMIRPUTRRAPI_TOKEN'),
 ];
