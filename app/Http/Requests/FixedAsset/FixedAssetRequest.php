b2591497df8a70e057edebdeed1fd38795f11861
Justine Dela Rosa <justinedelarosa06@gmail.com>
2024-10-14 08:44:48 +0800

    Disable validation for account_title_id
    Comment out the account_title_id validation rule in FixedAssetRequest. This change temporarily suspends the requirement for account_title_id to exist in the account_titles table.

--- app/Http/Requests/FixedAsset/FixedAssetRequest.php
+++ app/Http/Requests/FixedAsset/FixedAssetRequest.php
@@ -270,7 +270,7 @@ class FixedAssetRequest extends FormRequest
                 'unit_id' => ['required', 'exists:units,id', new UnitValidation(request()->department_id)],
                 'subunit_id' => ['required', 'exists:sub_units,id', new SubunitValidation(request()->unit_id, false)],
                 'location_id' => ['required', 'exists:locations,id', new LocationValidation(request()->subunit_id)],
-                'account_title_id' => 'required|exists:account_titles,id',
+//                'account_title_id' => 'required|exists:account_titles,id',
                 'uom_id' => 'nullable|exists:unit_of_measures,id',
             ];
         }
