16cbf81e6956f858be5a5e18eb8f96d1127d6a95
Justine Dela Rosa <justinedelarosa06@gmail.com>
2024-10-18 15:43:28 +0800

    Enhance CreateApiTokenRequest validation and rules
    Added validation to ensure the token does not already exist by decrypting stored tokens. Introduced unique and required validation rules for the endpoint field.

--- app/Http/Requests/ApiTokenRequest/CreateApiTokenRequest.php
+++ app/Http/Requests/ApiTokenRequest/CreateApiTokenRequest.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Requests\ApiTokenRequest;
 
+use App\Models\ApiToken;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
 
 class CreateApiTokenRequest extends FormRequest
 {
+
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -16,6 +21,7 @@ class CreateApiTokenRequest extends FormRequest
         return true;
     }
 
+
     /**
      * Get the validation rules that apply to the request.
      *
@@ -24,7 +30,26 @@ class CreateApiTokenRequest extends FormRequest
     public function rules()
     {
         return [
-            'token' => 'required|string',
+            'token' => [
+                'required',
+                'string',
+                function ($attribute, $value, $fail) {
+                    // Retrieve all tokens and check if the provided token matches any decrypted token
+                    $tokens = ApiToken::get(['token']);
+                    foreach ($tokens as $token) {
+                        try {
+                            if ($value == Crypt::decryptString($token->token)) {
+                                $fail('Token already exists');
+                                break;
+                            }
+                        } catch (\Exception $e) {
+                            // Handle decryption error (e.g., log the error, skip the token, etc.)
+                            $fail('Token decryption error');
+                        }
+                    }
+                }
+            ],
+            'endpoint' => 'required|string|unique:api_tokens,endpoint',
             'p_name' => ['required', 'string', 'max:255', 'unique:api_tokens,p_name']
         ];
     }
@@ -33,6 +58,8 @@ class CreateApiTokenRequest extends FormRequest
     {
         return [
             'token.required' => 'Token is required',
+            'endpoint.required' => 'Endpoint is required',
+            'endpoint.string' => 'Provide a valid endpoint',
             'p_name.required' => 'Project name is required',
             'p_name.unique' => 'Project name already exists'
         ];
