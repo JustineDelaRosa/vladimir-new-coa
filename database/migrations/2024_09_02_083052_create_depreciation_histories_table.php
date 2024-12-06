e491c5ba29302258b902d610036753266beae294
Justine Dela Rosa <justinedelarosa06@gmail.com>
2024-12-06 09:39:27 +0800

    add COA to depreciation history table for charge department history tracking

--- database/migrations/2024_09_02_083052_create_depreciation_histories_table.php
+++ database/migrations/2024_09_02_083052_create_depreciation_histories_table.php
@@ -23,8 +23,21 @@ class CreateDepreciationHistoriesTable extends Migration
             $table->double('remaining_book_value');
             $table->double('depreciation_basis');
             $table->double('acquisition_cost');
+            $table->unsignedInteger('company_id');
+            $table->unsignedInteger('business_unit_id');
+            $table->unsignedInteger('department_id');
+            $table->unsignedInteger('unit_id');
+            $table->unsignedInteger('subunit_id');
+            $table->unsignedInteger('location_id');
             $table->timestamps();
             $table->foreign('fixed_asset_id')->references('id')->on('fixed_assets')->onDelete('cascade');
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+            $table->foreign('business_unit_id')->references('id')->on('business_units')->onDelete('cascade');
+            $table->foreign('department_id')->references('id')->on('departments')->onDelete('cascade');
+            $table->foreign('unit_id')->references('id')->on('units')->onDelete('cascade');
+            $table->foreign('subunit_id')->references('id')->on('sub_units')->onDelete('cascade');
+            $table->foreign('location_id')->references('id')->on('locations')->onDelete('cascade');
+
         });
     }
 
