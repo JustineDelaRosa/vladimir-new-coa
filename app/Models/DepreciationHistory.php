0c0b34f9c4b93e7412a09854deaa50531d10f79f
Justine Dela Rosa <justinedelarosa06@gmail.com>
2025-01-16 08:18:11 +0800

    Add relationships to DepreciationHistory model
    Include additional belongsTo and hasMany relationships for the DepreciationHistory model to associate it with related entities such as Company, BusinessUnit, Department, and others. This enhances the model's ability to manage linked data efficiently.

--- app/Models/DepreciationHistory.php
+++ app/Models/DepreciationHistory.php
@@ -13,7 +13,40 @@ class DepreciationHistory extends Model
 
     public function fixedAsset()
     {
-      return $this->belongsTo(FixedAsset::class, 'fixed_asset_id', 'id');
+        return $this->belongsTo(FixedAsset::class, 'fixed_asset_id', 'id');
 
+    }
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class, 'company_id', 'id');
+    }
+
+    public function businessUnit()
+    {
+        return $this->belongsTo(BusinessUnit::class, 'business_unit_id', 'id');
+    }
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class, 'department_id', 'id');
+    }
+
+    public function unit()
+    {
+        return $this->belongsTo(Unit::class, 'unit_id', 'id');
+    }
+
+    public function subUnit()
+    {
+        return $this->belongsTo(SubUnit::class, 'subunit_id', 'id');
+    }
+
+    public function location(){
+        return $this->belongsTo(Location::class, 'location_id', 'id');
+    }
+
+    public function accountingEntries(){
+        return $this->hasMany(AccountingEntries::class, 'depreciation_id', 'id');
     }
 }
