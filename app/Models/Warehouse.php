5f41923bab1919685721345124951fb2d68626b8
Justine Dela Rosa <justinedelarosa06@gmail.com>
2025-03-06 17:02:23 +0800

    Refactor Warehouse model to update location and department relationships for improved data handling

--- app/Models/Warehouse.php
+++ app/Models/Warehouse.php
@@ -26,7 +26,26 @@ class Warehouse extends Model
         return $this->hasMany(User::class, 'warehouse_id', 'id');
     }
 
-    /*    public function location(){
-            return $this->belongsTo(Location::class, 'location_id', 'id');
-        }*/
+    public function location()
+    {
+        return $this->hasMany(Location::class, 'receiving_warehouse_id', 'sync_id');
+    }
+
+    public function department()
+    {
+        return $this->hasMany(Department::class, 'receiving_warehouse_id', 'sync_id');
+    }
+
+//    public function warehouseLocation()
+//    {
+//        return $this->belongsToMany(
+//            Location::class,
+//            'warehouse_location',
+//            'warehouse_id',
+//            'location_id',
+//            'sync_id',
+//            'sync_id'
+//        )->withTimestamps();
+//    }
+
 }
