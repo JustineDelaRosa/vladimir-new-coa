339ab6f7919eafbced1f7d1e730d108ddc64f548
Justine Dela Rosa <justinedelarosa06@gmail.com>
2025-03-12 16:58:38 +0800

    Refactor FileOrX validation to restrict allowed file types and improve error messaging

--- app/Rules/FileOrX.php
+++ app/Rules/FileOrX.php
@@ -19,13 +19,26 @@ class FileOrX implements Rule
     /**
      * Determine if the validation rule passes.
      *
-     * @param  string  $attribute
-     * @param  mixed  $value
+     * @param string $attribute
+     * @param mixed $value
      * @return bool
      */
     public function passes($attribute, $value)
     {
-        return is_file($value) || $value === 'x';
+        if ($value instanceof \Illuminate\Http\UploadedFile) {
+            $mime = $value->getMimeType();
+            return in_array($mime, [
+                'application/pdf',
+                'application/msword',
+                'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+                'application/vnd.ms-excel',
+                'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+/*                'image/jpeg',
+                'image/png',
+                'image/jpg',*/
+            ]);
+        }
+        return $value === 'x';
     }
 
     /**
@@ -35,6 +48,6 @@ class FileOrX implements Rule
      */
     public function message()
     {
-        return 'The :attribute must be a file or the string \'x\'.';
+        return 'Please check the file type of :attribute. Currently, only PDF, DOC, DOCX, XLS, and XLSX are allowed.';
     }
 }
